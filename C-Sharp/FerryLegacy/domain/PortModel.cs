1b402791160ecd3d5af30a54d35921f4ac6d9d9a
Víctor Pavón <victormpav@gmail.com>
2017-04-24 22:03:28 +0200

    Move PortModel to namespace domain

--- C-Sharp/FerryLegacy/domain/PortModel.cs
+++ C-Sharp/FerryLegacy/domain/PortModel.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using FerryLegacy.domain;
 
-namespace FerryLegacy
+namespace FerryLegacy.domain
 {
     public class PortModel
     {
